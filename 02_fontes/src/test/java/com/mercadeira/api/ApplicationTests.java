9f17837cb01878d9d080eeef42b728e900216717
lnrdgst <leo.afm@hotmail.com>
2026-08-29 16:32:54 -0300

    test: add PostgreSQL Testcontainers infrastructure

--- 02_fontes/src/test/java/com/mercadeira/api/ApplicationTests.java
+++ 02_fontes/src/test/java/com/mercadeira/api/ApplicationTests.java
@@ -2,9 +2,20 @@ package com.mercadeira.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(
+			DockerImageName.parse("postgres:18-alpine"));
 
 	@Test
 	void contextLoads() {
